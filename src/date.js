e446dcebaba81e7803f8c9f87a101d0b90097468
David Tumpowsky <tumpowskydavid@gmail.com>
2018-05-25 09:30:27 -0700

    initial set up with earth and date contstuctores

---
+++ src/date.js
@@ -0,0 +1,12 @@
+export class Date {
+
+  constructor (year, month, day, hours, minutes, seconds)
+  {
+    this.year=textHaiku;
+    this.month=month;
+    this.day=day;
+    this.hours=hours;
+    this.minutes=minutes;
+    this.seconds=secondes;
+  }
+}
---
+++ src/earthAge.js
@@ -0,0 +1,7 @@
+export class Earth {
+
+  constructor ()
+  {
+    this.textHaiku=textHaiku;
+  }
+}
